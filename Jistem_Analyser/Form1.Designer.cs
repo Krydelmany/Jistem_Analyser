d1dbf0a5ce24d025d0ccc03bf2403d72e725fa93
Giovani <Giovani@GIOVANI-PC>
2024-05-16 19:07:53 -0300

    Add project files.

---
+++ Jistem_Analyser/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace Jistem_Analyser
+{
+    partial class JystemAnalytics
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JystemAnalytics));
+            this.pnlButons = new System.Windows.Forms.Panel();
+            this.pnlNav = new System.Windows.Forms.Panel();
+            this.btnConfig = new System.Windows.Forms.Button();
+            this.btnVideo = new System.Windows.Forms.Button();
+            this.btnMemoria = new System.Windows.Forms.Button();
+            this.btnPlacaMae = new System.Windows.Forms.Button();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.pnlProfile = new System.Windows.Forms.Panel();
+            this.lbNomeSistema = new System.Windows.Forms.Label();
+            this.lbBemVindo = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlButons.SuspendLayout();
+            this.pnlProfile.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnlHeader.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlButons
+            // 
+            this.pnlButons.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.pnlButons.Controls.Add(this.pnlNav);
+            this.pnlButons.Controls.Add(this.btnConfig);
+            this.pnlButons.Controls.Add(this.btnVideo);
+            this.pnlButons.Controls.Add(this.btnMemoria);
+            this.pnlButons.Controls.Add(this.btnPlacaMae);
+            this.pnlButons.Controls.Add(this.btnDashboard);
+            this.pnlButons.Controls.Add(this.pnlProfile);
+            this.pnlButons.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlButons.Location = new System.Drawing.Point(2, 2);
+            this.pnlButons.Name = "pnlButons";
+            this.pnlButons.Size = new System.Drawing.Size(186, 558);
+            this.pnlButons.TabIndex = 0;
+            // 
+            // pnlNav
+            // 
+            this.pnlNav.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(243)))));
+            this.pnlNav.Location = new System.Drawing.Point(0, 193);
+            this.pnlNav.Name = "pnlNav";
+            this.pnlNav.Size = new System.Drawing.Size(3, 100);
+            this.pnlNav.TabIndex = 0;
+            // 
+            // btnConfig
+            // 
+            this.btnConfig.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnConfig.FlatAppearance.BorderSize = 0;
+            this.btnConfig.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConfig.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfig.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(243)))));
+            this.btnConfig.Image = ((System.Drawing.Image)(resources.GetObject("btnConfig.Image")));
+            this.btnConfig.Location = new System.Drawing.Point(0, 516);
+            this.btnConfig.Name = "btnConfig";
+            this.btnConfig.Size = new System.Drawing.Size(186, 42);
+            this.btnConfig.TabIndex = 1;
+            this.btnConfig.Text = " Configurações";
+            this.btnConfig.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnConfig.UseVisualStyleBackColor = true;
+            this.btnConfig.Click += new System.EventHandler(this.btnConfig_Click);
+            this.btnConfig.Leave += new System.EventHandler(this.btnConfig_Leave);
+            // 
+            // btnVideo
+            // 
+            this.btnVideo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnVideo.FlatAppearance.BorderSize = 0;
+            this.btnVideo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVideo.Font = new System.Drawing.Font("Cascadia Mono PL SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVideo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(243)))));
+            this.btnVideo.Image = ((System.Drawing.Image)(resources.GetObject("btnVideo.Image")));
+            this.btnVideo.Location = new System.Drawing.Point(0, 299);
+            this.btnVideo.Name = "btnVideo";
+            this.btnVideo.Size = new System.Drawing.Size(186, 42);
+            this.btnVideo.TabIndex = 1;
+            this.btnVideo.Text = " Vídeo    ";
+            this.btnVideo.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnVideo.UseVisualStyleBackColor = true;
+            this.btnVideo.Click += new System.EventHandler(this.btnVideo_Click);
+            this.btnVideo.Leave += new System.EventHandler(this.btnVideo_Leave);
+            // 
+            // btnMemoria
+            // 
+            this.btnMemoria.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMemoria.FlatAppearance.BorderSize = 0;
+            this.btnMemoria.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMemoria.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMemoria.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(243)))));
+            this.btnMemoria.Image = ((System.Drawing.Image)(resources.GetObject("btnMemoria.Image")));
+            this.btnMemoria.Location = new System.Drawing.Point(0, 257);
+            this.btnMemoria.Name = "btnMemoria";
+            this.btnMemoria.Size = new System.Drawing.Size(186, 42);
+            this.btnMemoria.TabIndex = 1;
+            this.btnMemoria.Text = " Memória  ";
+            this.btnMemoria.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnMemoria.UseVisualStyleBackColor = true;
+            this.btnMemoria.Click += new System.EventHandler(this.btnMemoria_Click);
+            this.btnMemoria.Leave += new System.EventHandler(this.btnMemoria_Leave);
+            // 
+            // btnPlacaMae
+            // 
+            this.btnPlacaMae.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPlacaMae.FlatAppearance.BorderSize = 0;
+            this.btnPlacaMae.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlacaMae.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlacaMae.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(243)))));
+            this.btnPlacaMae.Image = ((System.Drawing.Image)(resources.GetObject("btnPlacaMae.Image")));
+            this.btnPlacaMae.Location = new System.Drawing.Point(0, 215);
+            this.btnPlacaMae.Name = "btnPlacaMae";
+            this.btnPlacaMae.Size = new System.Drawing.Size(186, 42);
+            this.btnPlacaMae.TabIndex = 1;
+            this.btnPlacaMae.Text = " Placa-Mãe";
+            this.btnPlacaMae.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnPlacaMae.UseVisualStyleBackColor = true;
+            this.btnPlacaMae.Click += new System.EventHandler(this.btnPlacaMae_Click);
+            this.btnPlacaMae.Leave += new System.EventHandler(this.btnPlacaMae_Leave);
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDashboard.FlatAppearance.BorderSize = 0;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashboard.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDashboard.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(243)))));
+            this.btnDashboard.Image = ((System.Drawing.Image)(resources.GetObject("btnDashboard.Image")));
+            this.btnDashboard.Location = new System.Drawing.Point(0, 173);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(186, 42);
+            this.btnDashboard.TabIndex = 1;
+            this.btnDashboard.Text = " Início  ";
+            this.btnDashboard.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnDashboard.UseVisualStyleBackColor = true;
+            this.btnDashboard.Click += new System.EventHandler(this.btnDashboard_Click);
+            this.btnDashboard.Leave += new System.EventHandler(this.btnDashboard_Leave);
+            // 
+            // pnlProfile
+            // 
+            this.pnlProfile.Controls.Add(this.lbNomeSistema);
+            this.pnlProfile.Controls.Add(this.lbBemVindo);
+            this.pnlProfile.Controls.Add(this.pictureBox1);
+            this.pnlProfile.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlProfile.Location = new System.Drawing.Point(0, 0);
+            this.pnlProfile.Name = "pnlProfile";
+            this.pnlProfile.Size = new System.Drawing.Size(186, 173);
+            this.pnlProfile.TabIndex = 0;
+            // 
+            // lbNomeSistema
+            // 
+            this.lbNomeSistema.AutoSize = true;
+            this.lbNomeSistema.Font = new System.Drawing.Font("Cascadia Mono", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNomeSistema.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(243)))));
+            this.lbNomeSistema.Location = new System.Drawing.Point(25, 122);
+            this.lbNomeSistema.Name = "lbNomeSistema";
+            this.lbNomeSistema.Size = new System.Drawing.Size(40, 17);
+            this.lbNomeSistema.TabIndex = 1;
+            this.lbNomeSistema.Text = "User";
+            // 
+            // lbBemVindo
+            // 
+            this.lbBemVindo.AutoSize = true;
+            this.lbBemVindo.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbBemVindo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbBemVindo.Location = new System.Drawing.Point(23, 99);
+            this.lbBemVindo.Name = "lbBemVindo";
+            this.lbBemVindo.Size = new System.Drawing.Size(96, 23);
+            this.lbBemVindo.TabIndex = 2;
+            this.lbBemVindo.Text = "Bem-vindo";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(52, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(67, 59);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.pnlHeader.Controls.Add(this.pictureBox2);
+            this.pnlHeader.Controls.Add(this.btnMinimize);
+            this.pnlHeader.Controls.Add(this.btnClose);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(1, 0);
+            this.pnlHeader.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(793, 39);
+            this.pnlHeader.TabIndex = 2;
+            this.pnlHeader.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlHeader_Paint);
+            this.pnlHeader.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlHeader_MouseDown);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Enabled = false;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(3, -4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(166, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnMinimize.FlatAppearance.BorderSize = 0;
+            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimize.Image = ((System.Drawing.Image)(resources.GetObject("btnMinimize.Image")));
+            this.btnMinimize.Location = new System.Drawing.Point(691, 0);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(51, 39);
+            this.btnMinimize.TabIndex = 1;
+            this.btnMinimize.UseVisualStyleBackColor = true;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Image = ((System.Drawing.Image)(resources.GetObject("btnClose.Image")));
+            this.btnClose.Location = new System.Drawing.Point(742, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(51, 39);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            this.btnClose.MouseEnter += new System.EventHandler(this.btnClose_MouseEnter);
+            this.btnClose.MouseLeave += new System.EventHandler(this.btnClose_MouseLeave);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.panel2.Controls.Add(this.pnlHeader);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(188, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(794, 558);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1, 558);
+            this.panel1.TabIndex = 0;
+            // 
+            // JystemAnalytics
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(59)))), ((int)(((byte)(59)))));
+            this.ClientSize = new System.Drawing.Size(984, 562);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pnlButons);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "JystemAnalytics";
+            this.Padding = new System.Windows.Forms.Padding(2);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.pnlButons.ResumeLayout(false);
+            this.pnlProfile.ResumeLayout(false);
+            this.pnlProfile.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnlHeader.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlButons;
+        private System.Windows.Forms.Panel pnlProfile;
+        private System.Windows.Forms.Label lbNomeSistema;
+        private System.Windows.Forms.Button btnVideo;
+        private System.Windows.Forms.Button btnMemoria;
+        private System.Windows.Forms.Button btnPlacaMae;
+        private System.Windows.Forms.Button btnConfig;
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.Button btnDashboard;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lbBemVindo;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel pnlNav;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
